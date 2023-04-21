5b7db6fc0cdbcb9a5927dcab40b8186b2c274b7f
Liam Hodges <liamhodges@liams-mbp.mynetworksettings.com>
2023-04-20 23:06:17 -0400

    working without style

--- script.js
+++ script.js
@@ -1,19 +1,49 @@
-async function fetchData(){
+document.addEventListener('DOMContentLoaded', async () => {
+    const body = document.querySelector('body');
+    const news = new Messages();
+  });
 
-    const options = {
-        method: 'GET',
-        headers: {
-            'X-RapidAPI-Key': 'b59b05111emsh072b9d3685b2611p1deed7jsnfbf2bf3d7ddd',
-            'X-RapidAPI-Host': 'apidojo-yahoo-finance-v1.p.rapidapi.com'
-        }
-    };
+
+class Messages {
+    constructor() {
+      this.node = document.querySelector('.newsStories')
+      this.SPEED = 10000;
+      setTimeout(this.fetchData.bind(this), this.SPEED);
+    }
     
-    
+    async fetchData(){
+        const options = {
+            method: 'GET',
+            headers: {
+                'X-RapidAPI-Key': 'b59b05111emsh072b9d3685b2611p1deed7jsnfbf2bf3d7ddd',
+                'X-RapidAPI-Host': 'apidojo-yahoo-finance-v1.p.rapidapi.com'
+            }
+        };
+        fetch('https://apidojo-yahoo-finance-v1.p.rapidapi.com/auto-complete?q=tesla&region=US', options)
+            .then(response => response.json())
+            .then(response => {
+                response.news.forEach(article =>{
+                    const newsArticle = document.createElement('div');
+                    newsArticle.setAttribute('class', 'newsArticle');
+                    // append to message
+                    
+                    const text = document.createElement('a');
+                    const link = article.link
+                    text.setAttribute('href',link);
+                    text.innerHTML = `${article.title}`
 
-    const res = await fetch('https://apidojo-yahoo-finance-v1.p.rapidapi.com/auto-complete?q=tesla&region=US', options)
-    const record = await res.json()
 
-    console.log('record', record)
+                    const publisher = document.createElement('div');
+                    publisher.setAttribute('class', 'publisher');
 
-}
-fetchData()
+                    publisher.innerText = article.publisher
+
+                    newsArticle.appendChild(publisher);
+                    newsArticle.appendChild(text);
+                    this.node.appendChild(newsArticle);
+                })
+            })
+            .catch(err => console.error(err));
+            setTimeout(this.fetchData.bind(this), this.SPEED);
+            } 
+          }
